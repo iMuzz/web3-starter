c4a8138b509740802796defc472e5f0f3603b15d
Faraaz Nishtar <faraaznishtar@gmail.com>
2021-10-28 04:54:04 -0400

    bugfix: btn disabled state not showing up

--- components/button.tsx
+++ components/button.tsx
@@ -27,16 +27,14 @@ const Button: React.FC<IButtonProps> = ({
     <button
       type="button"
       disabled={isLoading || disabled}
-      className={classnames(
-        `transition-all duration-200 bg-indigo-500 hover:bg-indigo-600 text-${textColor} py-2 px-4 rounded font-medium text-center`,
-        {
-          'text-xs': size === 'sm',
-          'text-md': size === 'md',
-          'text-xl': size === 'lg',
-          'bg-gray-400 cursor-not-allowed': isLoading || disabled,
-          [`${classOverrides}`]: classOverrides?.length && classOverrides?.length > 0,
-        },
-      )}
+      className={classnames(`transition-all duration-200 text-${textColor} py-2 px-4 rounded font-medium text-center`, {
+        'text-xs': size === 'sm',
+        'text-md': size === 'md',
+        'text-xl': size === 'lg',
+        'bg-indigo-500 hover:bg-indigo-600': !isLoading,
+        'bg-gray-400 cursor-not-allowed': isLoading || disabled,
+        [`${classOverrides}`]: classOverrides?.length && classOverrides?.length > 0,
+      })}
       {...props}
     >
       {children}
